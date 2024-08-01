d86ffa55a26de8a289254a14a7a70c65bd1db859
juanscaffo <juanscaffo@hotmail.com>
2024-08-01 18:01:22 -0300

    primer commit

---
+++ PI-G3/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header"
+
+function App() {
+ 
+
+  return (
+    <>
+  <div>
+    <Header></Header>
+  </div>
+    </>
+  )
+}
+
+export default App
---
+++ PI-G3/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = () => {
+  return (
+    <div className='container-header'>
+        <div className="img">
+            <img src="/PI-G3/public/img/Logo.jpg" alt="logo" />
+        </div>
+        <div className="container-button">
+            <Button>Iniciar Sesion</Button>
+            <Button>Crear Cuenta</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
