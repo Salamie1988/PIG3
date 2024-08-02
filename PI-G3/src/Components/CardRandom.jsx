99c48436a6513f1e72902b5eca2c9b777c15305c
salamie1988 <salamierodrigo@gmail.com>
2024-08-02 20:47:32 -0300

    actualizando estilos

--- PI-G3/src/Components/CardRandom.jsx
+++ PI-G3/src/Components/CardRandom.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 
 const CardRandom = () => {
   return (
-    <div >
+    <div className='container-tours-random'>
+      <div className='img-random'>
         <img className='img-card-random' src="https://cdn5.travelconline.com/unsafe/fit-in/2000x0/filters:quality(75):format(webp)/https%3A%2F%2Ftr2storage.blob.core.windows.net%2Fimagenes%2FNLzrVg9ieSiY2fEOUW-VmVLMeWrIPjpeg.jpeg" alt="bariloche" />
-        <h2 className='title-card-random'>BARILOCHE</h2>
+      </div>
+      <div className='titulo'>
+        <h4 className='title-card-random'>BARILOCHE</h4>
+      </div>
     </div>
+
   )
 }
 
